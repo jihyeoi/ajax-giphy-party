920d2d87db41e37526e068b86de4cdab1600ce6d
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-02-13 16:30:09 -0800

    completed index.html, worked oon app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Let's get this party started!");
+$("#retrieve").on("submit", function(evt) {
+  evt.preventDefault();
+
+  async function getKeyword() {
+    let keyword = $("#keyword").val();
+
+    const key = As4tMmZIYRlRryZXDDKGjSWhqSMm5Qh5;
+    const params = new URLSearchParams({keyword});
+    let response = await fetch(`api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${key}`)
+
+    console.log(response)
+
+  }
+
+  getKeyword();
+})
