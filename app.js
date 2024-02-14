ecc1a36faf009b50a76f9f0be99b60c736bd7b9a
James Lee <jimhlee23@gmail.com>
2024-02-13 20:16:59 -0500

    getKeyword mostly complete

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-$("#retrieve").on("submit", function(evt) {
+'use strict';
+
+const gifArea = $('#gifs');
+
+$("#giphy-form").on("submit", function (evt) {
   evt.preventDefault();
-
-  async function getKeyword() {
-    let keyword = $("#keyword").val();
-
-    const key = As4tMmZIYRlRryZXDDKGjSWhqSMm5Qh5;
-    const params = new URLSearchParams({keyword});
-    let response = await fetch(`api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${key}`)
-
-    console.log(response)
-
-  }
-
   getKeyword();
-})
+
+});
+
+/** Gets keyword from form */
+async function getKeyword() {
+  let keyword = $("#keyword").val();
+
+  const key = 'As4tMmZIYRlRryZXDDKGjSWhqSMm5Qh5';
+  const params = new URLSearchParams({ keyword, api_key: key });
+  // response is the response object
+  const response = await fetch(
+    `http://api.giphy.com/v1/gifs/search?${params}`);
+  const jsonthing = await response.json()
+  console.log(jsonthing);
+  console.log(response)
+}
+
+function appendGif(responseObj) {
+  const retrievedGif =  responseObj.images.
+  gifArea.append(retrievedGif)
+}
