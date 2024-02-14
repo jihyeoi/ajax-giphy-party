2d9ba441f95f2469b1c83a709d033fb643a38025
James Lee <jimhlee23@gmail.com>
2024-02-13 20:30:30 -0500

    fixed the query field in fetch

--- app.js
+++ app.js
@@ -13,16 +13,18 @@ async function getKeyword() {
   let keyword = $("#keyword").val();
 
   const key = 'As4tMmZIYRlRryZXDDKGjSWhqSMm5Qh5';
-  const params = new URLSearchParams({ keyword, api_key: key });
+  const params = new URLSearchParams({ q: keyword, api_key: key });
   // response is the response object
   const response = await fetch(
     `http://api.giphy.com/v1/gifs/search?${params}`);
   const jsonthing = await response.json()
+
   console.log(jsonthing);
-  console.log(response)
 }
 
 function appendGif(responseObj) {
   const retrievedGif =  responseObj.images.
   gifArea.append(retrievedGif)
 }
+
+'cfuL5gqFDreXxkWQ4o'
